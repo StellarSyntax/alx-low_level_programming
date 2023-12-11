fdf9459fe85977e907b14412d460e29460f91c06
StellarSyntax <henrysai10@icloud.com>
2023-12-11 03:59:08 -0800

    all fesh

---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *sub - sub two integers
+ * @a: first int
+ * @b: second int
+ * Return: subtracted final result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
